88594cff1d5dbf1a73f1d6d6374f265f18877777
Eduardo <tkd13000@gmail.com>
2024-04-05 19:48:40 -0600

    Location, backgrounds and waves are set up

---
+++ time.js
@@ -0,0 +1,33 @@
+function getTime() {
+  fetch('http://worldtimeapi.org/api/ip')
+    .then(response => response.json())
+    .then(data => {
+      const horaActual = new Date(data.datetime);
+      const horaFormatoLocal = horaActual.getHours();
+      document.getElementById('current-time').textContent = horaActual.toLocaleTimeString();
+      const smallImage = document.getElementById('small-image');
+      const wave = document.getElementById('wave');
+      // Cambiar el fondo de pantalla según la hora del día
+      if (horaFormatoLocal >= 6 && horaFormatoLocal < 18) 
+      {
+          document.body.style.backgroundImage = "url('./Imagenes/bg-image-daytime.jpg')";
+          smallImage.src = "./Imagenes/bg-image-daytime.jpg";
+          wave.textContent = "GOOD MORNING, IT´S CURRENTLY";
+      }
+
+      else
+      {
+          document.body.style.backgroundImage = "url('./Imagenes/bg-image-nighttime.jpg')";
+          smallImage.src = "./Imagenes/icon-moon.svg";
+          wave.textContent = "GOOD NIGHT, IT´S CURRENTLY";
+      }
+    })
+    .catch(error => console.error('Hubo un error al obtener la hora:', error));
+}
+
+// Llamar a la función getTime() inicialmente
+getTime();
+
+// Actualizar la hora cada segundo
+setInterval(getTime, 1000);
+
--- ubicacion.js
+++ ubicacion.js
@@ -3,20 +3,17 @@ const token = 'f8bc1c28432648';
 fetch(`https://ipinfo.io/json?token=${token}`)
   .then(response => response.json())
   .then(data => {
-    console.log(data); // Imprime la respuesta completa para examinar su estructura
+    // console.log(data); // Imprime la respuesta completa para examinar su estructura
     
     const city = data.city;
     const state = data.region; // Accede al campo 'region' para obtener el estado
-    const country = data.country;
-    const postalCode = data.postal;
-    const coordinates = data.loc ? data.loc.split(',') : null;
+    // const country = data.country;
+    // const postalCode = data.postal;
+    // const coordinates = data.loc ? data.loc.split(',') : null;
     
-    console.log('Ciudad:', city);
+
     document.getElementById('city').textContent = city;
-    console.log('Estado:', state);
-    document.getElementById('state').textContent = state;
-    console.log('País:', country);
-    console.log('Código postal:', postalCode);
-    console.log('Coordenadas:', coordinates);
+    document.getElementById('state').textContent = state + ",";
+
   })
   .catch(error => console.error('Hubo un error al obtener la ubicación:', error));
