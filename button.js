58c43e57fa30b96312d31932503d9c235ca87fae
Eduardo <tkd13000@gmail.com>
2024-04-07 17:29:21 -0600

    Changer button

--- button.js
+++ button.js
@@ -1,15 +1,16 @@
 document.getElementById("toggleButton").addEventListener("click", function() {
-    var text = document.getElementById("toggleText");
-    var icon = document.getElementById("toggleIcon");
-  
-    if (text.innerHTML === "More") {
-      text.innerHTML = "Less";
-      icon.classList.add("rotated");
-    } else {
-      text.innerHTML = "More";
-      icon.classList.remove("rotated");
-    }
-  });
+  let text = document.getElementById("toggleText");
+  let icon = document.getElementById("toggleIcon");
+
+  if (text.innerHTML === "More") {
+    text.innerHTML = "Less";
+    icon.innerHTML = "&#9650;"; 
+  } else {
+    text.innerHTML = "More";
+    icon.innerHTML = "&#9660;";
+  }
+});
+
   
   
   
